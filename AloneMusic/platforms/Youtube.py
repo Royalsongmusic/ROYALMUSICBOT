473148a2e3a00f2c6a45691d7d028fb8aa388762
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2025-09-30 14:17:49 +0530

    Update Youtube.py

--- AloneMusic/platforms/Youtube.py
+++ AloneMusic/platforms/Youtube.py
@@ -8,7 +8,7 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from AloneMusic.utils.database import is_on_off
+from AloneMusic.utils.database import is
 from AloneMusic.utils.formatters import time_to_seconds
 import os
 import glob
